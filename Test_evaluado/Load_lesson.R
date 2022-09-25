e56335ad177c31c5b34fdda2cc9a721f266570d0
Jose Daniel Conejeros <jd@MacBook-Pro-de-Jose.local>
2022-09-25 20:47:51 -0300

    Test de código evaluado (20%)

---
+++ Test_evaluado/Load_lesson.R
@@ -0,0 +1,16 @@
+# Instalamos las librerías para cargar el curso
+install.packages(c('swirl', 'swirlify'))
+
+# Cargamos las librerías en la sesión de trabajo
+library(swirl)
+library(swirlify)
+
+# Instalamos el curso
+install_course()
+
+#Cargamos el curso
+swirl()
+
+# Debes selecionar para seguir 
+
+# Presione 0 o "escape" para salir del curso una vez enviada la respuesta.
